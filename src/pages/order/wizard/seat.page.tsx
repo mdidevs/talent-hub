df84c039ba2b31cf51207d15e3b7a50e6cb0b3fc
Ragavarman <ragav@mdidev.com>
2026-02-15 23:59:53 +0530

    Rename order folder

---
+++ src/pages/order/wizard/seat.page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import SeatView from '@/views/order/wizard/seat.view'
+
+
+const SeatPage: React.FC = () => {
+	return (
+        <SeatView/>
+    )
+}
+
+export default SeatPage
